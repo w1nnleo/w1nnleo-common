f91e9d39b2c19fd6cd354647379e90841de0f672
w1nnleo <w1nnleo@163.com>
2023-04-05 10:51:59 +0800

    fix(exception): 统一异常处理打印错误

--- w1nnleo-common-exception/src/main/java/com/w1nnleo/exception/handler/GlobalCustomAdvice.java
+++ w1nnleo-common-exception/src/main/java/com/w1nnleo/exception/handler/GlobalCustomAdvice.java
@@ -1,5 +1,6 @@
 package com.w1nnleo.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,32 +16,33 @@ import com.w1nnleo.exception.entity.BusinessException;
  * @date: 2023/2/15
  * @Description:
  */
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalCustomAdvice {
 
     @ExceptionHandler(BusinessException.class)
     public R<?> handler(BusinessException e) {
+        log.error("BusinessException is error:[{}]",e.getMessage(),e);
         String[] split = e.getMessage().split(Constant.COMMA);
         return R.fail(Integer.valueOf(split[0]), split[1]);
     }
 
     @ExceptionHandler(Exception.class)
     public R<?> handler(Exception e) {
-        e.printStackTrace();
+        log.error("Exception is error:[{}]",e.getMessage(),e);
         return R.fail(CommonEnum.FAIL.getCode(), CommonEnum.FAIL.getMsg());
     }
 
     @ExceptionHandler(BindException.class)
     public R<?> bindExceptionHandler(BindException e) {
-        e.printStackTrace();
+        log.error("BindException is error:[{}]",e.getMessage(),e);
         FieldError error = e.getFieldError();
         return R.fail(CommonEnum.FAIL.getCode(), error.getDefaultMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public R<?> exceptionHandler(MethodArgumentNotValidException e) {
-        e.printStackTrace();
+        log.error("MethodArgumentNotValidException is error:[{}]",e.getMessage(),e);
         FieldError error = e.getBindingResult().getFieldError();
         return R.fail(CommonEnum.FAIL.getCode(), error.getDefaultMessage());
     }
